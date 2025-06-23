03aa71669c29ce0eccd6fb9ca8d1bdd218a4b9aa
buddhasystem <potekhin@bnl.gov>
2025-06-23 09:57:20 -0400

    Bug fix (factor type)

--- test/sim_test.py
+++ test/sim_test.py
@@ -11,7 +11,7 @@ import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose",  action='store_true',    help="Verbose mode")
 parser.add_argument("-s", "--schedule", type=str,               help='Path to the schedule (YAML)', default='')
-parser.add_argument("-f", "--factor", type=str,                 help='Time factor',                 default=1.0)
+parser.add_argument("-f", "--factor",   type=float,             help='Time factor',                 default=1.0)
 
 args        = parser.parse_args()
 verbose     = args.verbose
@@ -39,7 +39,7 @@ if verbose:
 # Python path is set, import the sim package:
 from  sim import *
 
-daq = DAQ(schedule_f = schedule, verbose=verbose)
+daq = DAQ(schedule_f = schedule, factor = factor, verbose = verbose)
 daq.simulate()
 
 print('---')

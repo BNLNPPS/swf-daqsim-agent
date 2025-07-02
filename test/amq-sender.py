eebc5f009095adb21a78fe7b84095ba64d95ef05
xzhao87 <xzhao87@gmail.com>
2025-07-02 15:43:50 -0400

    example scripts for sending and receiving messages through activemq-artemis multicast address

---
+++ test/amq-sender.py
@@ -0,0 +1,22 @@
+import stomp
+import ssl
+
+host = 'pandaserver02.sdcc.bnl.gov'
+port = 61612
+cafile = '<pathname to where full-chain.pem is>'
+
+conn = stomp.Connection(host_and_ports=[(host, port)], vhost=host,try_loopback_connect=False)
+
+conn.transport.set_ssl(
+    for_hosts=[(host, port)],
+    ca_certs=cafile,
+    ssl_version=ssl.PROTOCOL_TLS_CLIENT
+)
+
+try:
+    conn.connect(login='<username>', passcode='<userpasswd>', wait=True, version='1.2')
+    conn.send(destination='epictopic', body='Hello from producer!')
+    print("Message sent")
+    conn.disconnect()
+except Exception as e:
+    print("Connection failed:", type(e).__name__, e)
